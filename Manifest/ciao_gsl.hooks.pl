42568d34d0bae00729345511a24eb757d23d207b
Jose F. Morales <jfmcjf@gmail.com>
2017-03-08 19:32:09 +0100

    Public snapshot for 1a1b4efe15724b5653770b62fcdf678d1711629b
    Src-commit: 1a1b4efe15724b5653770b62fcdf678d1711629b

--- Manifest/ciao_gsl.hooks.pl
+++ Manifest/ciao_gsl.hooks.pl
@@ -7,12 +7,11 @@
 
 :- use_module(ciaobld(messages_aux), [normal_message/2]).
 
-:- use_module(library(file_utils), [string_to_file/2]).
 :- use_module(library(pathnames), [path_relocate/4, path_concat/3]).
 :- use_module(library(system_extra), [mkpath/1]).
 :- use_module(library(bundle/bundle_flags), [get_bundle_flag/2]).
 :- use_module(library(bundle/bundle_paths), [bundle_path/3]).
-:- use_module(ciaobld(third_party_config), [foreign_config_var/4]).
+:- use_module(ciaobld(third_party_config), [foreign_config_atmlist/4]).
 :- use_module(library(system), [find_executable/2]).
 
 m_bundle_foreign_config_tool(ciao_gsl, gsl, 'gsl-config').
@@ -37,8 +36,6 @@ third_party_preinstalled(ciao_gsl) :-
     build_system(gnu_build_system)
 ]).
 
-:- use_module(library(llists), [flatten/2]).
-
 enabled := ~get_bundle_flag(ciao_gsl:enabled).
 auto_install := ~get_bundle_flag(ciao_gsl:auto_install).
 
@@ -47,8 +44,9 @@ auto_install := ~get_bundle_flag(ciao_gsl:auto_install).
 	prepare_bindings.
 
 :- use_module(ciaobld(third_party_install), [auto_install/2]).
-:- use_module(ciaobld(eng_defs), [eng_path/3]).
 :- use_module(ciaobld(builder_aux), [add_rpath/3]).
+:- use_module(ciaobld(builder_aux), [update_file_from_clauses/3]).
+:- use_module(ciaobld(builder_aux), [update_stat_config_sh/2]).
 
 do_auto_install :-
 	( auto_install(yes) -> 
@@ -60,10 +58,9 @@ do_auto_install :-
 prepare_bindings :-
 	( enabled(yes) ->
 	    normal_message("configuring GSL library", []),
-	    S = ":- include(ciao_gsl(gsl_ciao)).\n",
- 	    foreign_config_var(ciao_gsl, gsl, 'cflags', CompilerOpts),
- 	    foreign_config_var(ciao_gsl, gsl, 'libs', LinkerOpts0),
-	    fix_linker_opts(LinkerOpts0, LinkerOpts1),
+	    S = [(:- include(ciao_gsl(gsl_ciao)))],
+ 	    foreign_config_atmlist(ciao_gsl, gsl, 'cflags', CompilerOpts),
+ 	    foreign_config_atmlist(ciao_gsl, gsl, 'libs', LinkerOpts1),
 	    ( auto_install(yes) ->
 	        % If installed as a third party, add ./third-party/lib
 	        % to the runtime library search path
@@ -71,39 +68,21 @@ prepare_bindings :-
 	    ; LinkerOpts2 = LinkerOpts1
 	    ),
 	    add_rpath(executable_path, LinkerOpts2, LinkerOpts),
-	    T = ~flatten([
-		    ":- extra_compiler_opts(\'"||CompilerOpts, "\').\n"||
-		    ":- extra_linker_opts(\'"||LinkerOpts, "\').\n"]),
-	    string_to_file(T, ~bundle_path(ciao_gsl, 'src/gsl_ciao_decl_auto.pl')),
-	    % List of static libraries from GSL
-	    % TODO: generalize for any other library
-	    M = ~flatten(["ADD_STAT_LIBS=\'"||LinkerOpts, "\'\n"])
-	;
-	    LinkerOpts = "",
-	    normal_message("ignoring GSL library", []),
-	    S = ":- include(ciao_gsl(gsl_ciao_dummy)).\n",
-	    M = ""
+	    update_file_from_clauses([
+		(:- extra_compiler_opts(CompilerOpts)),
+		(:- extra_linker_opts(LinkerOpts))
+              ], ~bundle_path(ciao_gsl, 'src/gsl_ciao_decl_auto.pl'), _)
+	; normal_message("ignoring GSL library", []),
+	  LinkerOpts = [],
+	  S = [(:- include(ciao_gsl(gsl_ciao_dummy)))]
 	),
+	%
+	update_file_from_clauses(S, ~bundle_path(ciao_gsl, 'src/ciao_gsl_auto.pl'), _),
+	%
         % TODO: See 'static_engine' at engine.hooks.pl for an example
         %   of engine that links against this library statically
 	%
 	% TODO: Simplify, generalize for other libs
 	%
-	EngOpts = [],
-	GSLEng = eng_def(core, 'gsl', EngOpts),
-	GSLCfgDir = ~eng_path(cfgdir, GSLEng), % NOTE: not an engine
-	mkpath(GSLCfgDir),
-	string_to_file(M, ~path_concat(GSLCfgDir, 'config_sh')),
-	string_to_file(S, ~bundle_path(ciao_gsl, 'src/ciao_gsl_auto.pl')).
-
-:- use_module(library(lists), [append/3]).
-
-% Remove the -L option, hack that allows to run in LINUXi686 or LINUXx86_64 --EMM:
-fix_linker_opts(LinkerOpts0, LinkerOpts) :-
-	( get_platform('LINUXx86_64') 
-	; get_platform('LINUXi686') 
-	),
-	append("-L"||_, " "||LinkerOpts, LinkerOpts0),
-	!.
-fix_linker_opts(LinkerOpts, LinkerOpts).
-
+	GSLEng = eng_def(core, 'gsl', []), % NOTE: not really an engine
+	update_stat_config_sh(GSLEng, LinkerOpts).
